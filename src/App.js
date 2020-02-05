ea6951f803709d6c9b8985b3fb85540ad3eab134
Leonardo Gonçalves <leonardo.goncalves@totvs.com.br>
2020-02-05 09:53:15 -0300

    useEffect, useMemo, useCallback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
   const [tech, setTech] = useState([
-    'ReactJS',
-    'React Native',
+    '',
   ]);
   const [newTech, setNewTech] = useState('');
 
-  function handleAdd() {
+  const handleAdd = useCallback(() => {
     setTech([...tech, newTech]);
     setNewTech('');
-  }
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
 
   return (
     <>
@@ -19,6 +33,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
       <input value={newTech} type="text" onChange={(e) => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </>
